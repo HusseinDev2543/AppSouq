9d23787d5a32c6952d9515d7656486441920f3e6
HusseinDev2543 <he557hh@gmail.com>
2025-01-26 12:25:01 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAySsu-6Hgt07j4JOcjLryo-v0ECwGUIb8",
+    authDomain: "Authjk",
+    projectId: "appsouq-9714b",
+    storageBucket: "appsouq-9714b.appspot.com",
+    messagingSenderId: "437915304190",
+    appId: "1:437915304190:android:e28af3f65c55e1c60ae5af",
+    databaseURL: "https://appsouq-9714b-default-rtdb.firebaseio.com"
+};
+
+// تهيئة Firebase
+firebase.initializeApp(firebaseConfig);
